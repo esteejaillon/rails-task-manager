b409827a5847f1cb4d6a7c31671a58876a926cf8
Estée Jaillon <e.jaillon@gmail.com>
2022-05-17 16:12:09 +0200

    exercise with CRUD done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # CREATE : 1) get a new task 2) post a new task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # READ: 1) list all 2) get all details for one
+  get "tasks", to: "tasks#index"
+
+  get "tasks/:id", to: "tasks#show", as: :task # le as: :task permet de définir le préfixe des routes
+
+  # UPDATE: 1) get the form 2) edit the form
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # DELETE: 1) destroy by id
+
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
+
 end
